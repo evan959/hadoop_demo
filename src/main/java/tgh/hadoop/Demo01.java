42217e656085f1f2938a3e13c7e04ed88b7b5eef
89006376 <89006376@sfmail.sf-express.com>
2018-12-27 18:42:57 +0800

    first commit

---
+++ src/main/java/tgh/hadoop/Demo01.java
@@ -0,0 +1,26 @@
+package tgh.hadoop;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class Demo01 {
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration config = new Configuration();
+        config.set("dfs.replication","1");
+
+        FileSystem fs = FileSystem.get(new URI("hdfs://119.23.191.109:8020"),config,"root");
+
+        fs.copyFromLocalFile(new Path("D:\\user\\89006376\\我的文档\\MeetNowInstall.log"),new Path("/java_test"));
+
+        System.out.println("HDFS上传文件成功");
+    }
+}
